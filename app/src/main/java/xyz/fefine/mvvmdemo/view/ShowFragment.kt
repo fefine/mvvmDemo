e26518dec09aa32a7673298ccfedc3bbdb746a0e
fefine <feng_sh@outlook.com>
2019-10-16 01:07:45 +0800

    databinding + livedata

--- app/src/main/java/xyz/fefine/mvvmdemo/view/ShowFragment.kt
+++ app/src/main/java/xyz/fefine/mvvmdemo/view/ShowFragment.kt
@@ -1,32 +1,45 @@
 package xyz.fefine.mvvmdemo.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.fragment_show.*
+import xyz.fefine.mvvmdemo.BR
 import xyz.fefine.mvvmdemo.R
 import xyz.fefine.mvvmdemo.viewmodel.ShowViewModel
 
 class ShowFragment: Fragment() {
 
     private lateinit var model: ShowViewModel
+    private lateinit var binding: ViewDataBinding
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         model = ViewModelProviders.of(this).get(ShowViewModel::class.java)
 
-        val nameObserver = Observer<String> {
-            newName -> txt_current_name.text = newName
-        }
+// --- start
+//        val nameObserver = Observer<String> {
+//            newName -> txt_current_name.text = newName
+//        }
 
         // https://proandroiddev.com/5-common-mistakes-when-using-architecture-components-403e9899f4cb
         // don't use this
-        model.currentName.observe(viewLifecycleOwner, nameObserver)
+//        model.currentName.observe(viewLifecycleOwner, nameObserver)
+// --- end
+
+        // 必须使用
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.setVariable(BR.mv, model)
+
+        Log.d("hua", "onActivityCreated")
     }
 
     override fun onCreateView(
@@ -34,13 +47,11 @@ class ShowFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_show, container, false)
-    }
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_show, container, false)
+        val view = binding.root
+        Log.d("hua", "onCreateView")
 
-    override fun onStart() {
-        super.onStart()
-
-        btn_update.setOnClickListener{ view -> model.currentName.value = "ming"}
+        return view
     }
 
 }
--- app/src/main/java/xyz/fefine/mvvmdemo/viewmodel/ShowViewModel.kt
+++ app/src/main/java/xyz/fefine/mvvmdemo/viewmodel/ShowViewModel.kt
@@ -1,14 +1,21 @@
 package xyz.fefine.mvvmdemo.viewmodel
 
+import android.util.Log
+import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ShowViewModel: ViewModel() {
+class ShowViewModel: ViewModel(), View.OnClickListener {
+    override fun onClick(p0: View?) {
+        Log.d("hua", "click")
+        this.currentName.value = "ming"
+    }
 
     val currentName: MutableLiveData<String> by lazy {
         MutableLiveData<String>("xiaohu")
     }
+
 
 
 }
